6216721eff35ffd94986b1ad6a5b57ccce688100
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-01-11 18:20:03 -0300

    tabuleiro montado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-const FRONT = "card_front"
-const back =  "bard_back"
+const FRONT = "card-front"
+const BACK =  "card-back"
+const CARD = "card"
+const ICON = "icon"
 
 
 let animals = [
@@ -22,8 +24,52 @@ startGame();
 function startGame(){
     cards = createCardsFromAnimals(animals);
     shuffleCards(cards);
-    console.log(cards);
+    
+    initializeCards(cards);
+    
+    
 }
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+    
+    
+    cards.forEach(card=>{
+
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement);
+
+    })
+    
+}
+
+function createCardContent(card, cardElement){
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+
+function createCardFace(face, card , element){
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face)
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/" + card.icon + ".jpg";
+        cardElementFace.appendChild(iconElement);
+    }else{
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace)
+}
+
 
 function shuffleCards(cards) {
     let currentIndex = cards.length;
@@ -43,9 +89,9 @@ function createCardsFromAnimals(animals){
 
     let cards = [];
 
-    for(let animal of animals){
+    animals.forEach( (animal) => {
         cards.push(createPairFromAnimals(animal));
-    }
+    })
 
     return cards.flatMap(pair=>pair)
 }
@@ -68,4 +114,8 @@ function createPairFromAnimals(animal){
 
 function createIdWithAnimal(animal){
     return animal + parseInt(Math.random() * 1000);
+}
+
+function flipCard(){
+    this.classList.add("flip")
 }

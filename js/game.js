638931ad50a5fbbd1da0a0a58bda0d043486edcf
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-01-11 23:09:58 -0300

    conferindo pares

--- js/game.js
+++ js/game.js
@@ -1,6 +1,40 @@
 let game = {
 
-    animals : [
+    lockMode: false,
+    fristCard: null,
+    secondCard: null,
+
+    setCard: function (id) {
+
+        let card = this.cards.filter(card => card.id === id)[0];
+
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.fristCard) {
+            this.fristCard = card;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function () {
+        return this.fristCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.fristCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+
+    },
+
+    animals: [
         'bear',
         'dog',
         'elephant',
@@ -13,25 +47,25 @@ let game = {
         'zebra',
     ],
 
-    cards : null,
+    cards: null,
 
-    createCardsFromAnimals: function() {
+    createCardsFromAnimals: function () {
 
         this.cards = [];
-    
-        this.animals.forEach( (animal) => {
+
+        this.animals.forEach((animal) => {
             this.cards.push(this.createPairFromAnimals(animal));
         })
-    
-        this.cards = this.cards.flatMap(pair=>pair)
+
+        this.cards = this.cards.flatMap(pair => pair)
         this.shuffleCards();
         return this.cards
     },
-    
-    
-    
-    createPairFromAnimals: function(animal) {
-        return[{
+
+
+
+    createPairFromAnimals: function (animal) {
+        return [{
             id: this.createIdWithAnimal(animal),
             icon: animal,
             flipped: false,
@@ -39,25 +73,25 @@ let game = {
             id: this.createIdWithAnimal(animal),
             icon: animal,
             flipped: false,
-         } ]
+        }]
     },
-    
-    
-    
-    createIdWithAnimal: function(animal){
+
+
+
+    createIdWithAnimal: function (animal) {
         return animal + parseInt(Math.random() * 1000);
     },
 
 
-    shuffleCards: function(cards) {
+    shuffleCards: function (cards) {
         let currentIndex = this.cards.length;
         let randomIndex = 0;
-    
-        while(currentIndex !== 0){
-    
+
+        while (currentIndex !== 0) {
+
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-    
+
             [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
         }
     }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const FRONT = "card-front"
-const BACK =  "card-back"
+const BACK = "card-back"
 const CARD = "card"
 const ICON = "icon"
 
@@ -10,17 +10,17 @@ const ICON = "icon"
 
 startGame();
 
-function startGame(){
-    
+function startGame() {
+
     initializeCards(game.createCardsFromAnimals());
-    
+
 }
 
 function initializeCards(cards) {
     let gameBoard = document.getElementById("gameBoard");
-    
-    
-    game.cards.forEach(card=>{
+
+
+    game.cards.forEach(card => {
 
         let cardElement = document.createElement('div')
         cardElement.id = card.id;
@@ -33,75 +33,54 @@ function initializeCards(cards) {
         gameBoard.appendChild(cardElement);
 
     })
-    
+
 }
 
-function createCardContent(card, cardElement){
+function createCardContent(card, cardElement) {
     createCardFace(FRONT, card, cardElement);
     createCardFace(BACK, card, cardElement);
 }
 
 
-function createCardFace(face, card , element){
+function createCardFace(face, card, element) {
     let cardElementFace = document.createElement('div');
     cardElementFace.classList.add(face)
-    if(face === FRONT){
+    if (face === FRONT) {
         let iconElement = document.createElement('img');
         iconElement.classList.add(ICON);
         iconElement.src = "./assets/" + card.icon + ".jpg";
         cardElementFace.appendChild(iconElement);
-    }else{
+    } else {
         cardElementFace.innerHTML = "&lt/&gt";
     }
     element.appendChild(cardElementFace)
 }
 
 
-/*function shuffleCards(cards) {
-    let currentIndex = cards.length;
-    let randomIndex = 0;
+function flipCard() {
+    if (game.setCard(this.id)) {
+        this.classList.add("flip")
 
-    while(currentIndex !== 0){
+        if (game.checkMatch()) {
 
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
+            game.clearCards();
 
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+        } else {
+
+            setTimeout(() => {
+
+                let fristCardView = document.getElementById(game.fristCard.id);
+                let SecondCardView = document.getElementById(game.secondCard.id);
+
+                fristCardView.classList.remove('flip');
+                SecondCardView.classList.remove('flip');
+                game.clearCards();
+            }, 1000);
+
+
+        }
     }
-}*/
-
-
-/*function createCardsFromAnimals(animals){
-
-    let cards = [];
-
-    animals.forEach( (animal) => {
-        cards.push(createPairFromAnimals(animal));
-    })
-
-    return cards.flatMap(pair=>pair)
 }
 
 
 
-function createPairFromAnimals(animal){
-    return[{
-        id: createIdWithAnimal(animal),
-        icon: animal,
-        flipped: false,
-    }, {
-        id: createIdWithAnimal(animal),
-        icon: animal,
-        flipped: false,
-     } ]
-}
-
-
-
-function createIdWithAnimal(animal){
-    return animal + parseInt(Math.random() * 1000);
-}*/
-
-function flipCard(){
-    this.classList.add("flip")
-}
